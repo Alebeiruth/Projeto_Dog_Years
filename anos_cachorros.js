00889dac22d9615130ba1dfadeb96a3c89bc58af
Alexandre Beiruth <alexandrebeyruth@gmail.com>
2022-05-25 16:09:49 -0300

    Pirmeiro Commit

---
+++ anos_cachorros.js
@@ -0,0 +1,28 @@
+// uma variavel (const) com minha idade.
+const myAge = 15;
+
+// uma variavel (let) e sua multiplicaçao por 10.5.
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// subtração da variavel por -2.
+
+let laterYears = myAge - 2;
+
+// multipicação da variavel por 4.
+laterYears = laterYears * 4;
+
+// chequando as variaveis 
+console.log(earlyYears)
+console.log(laterYears)
+
+// nova variavel idade de cachorro
+
+var myAgeInDogYears = earlyYears + laterYears
+
+// palavras em minusculo (toLowerCase)
+var Myname = "Ale";
+console.log("AleXandre".toLowerCase());
+
+//resultado 
+console.log(`My name is ${Myname}. I am ${myAgeInDogYears} years old in dog years`)
